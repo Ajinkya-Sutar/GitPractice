e985f64e7139fb1bc55e042878f4d76f69565c57
aj <ajinkyasutar064@gmail.com>
2022-12-04 14:23:26 +0530

    test and pages are updated

--- src/Pages/Dummy2.java
+++ src/Pages/Dummy2.java
@@ -4,6 +4,9 @@ public class Dummy2 {
 	
 	public void method1(){
 		System.out.println("Practice git commands");
+		int ab=5;
+		String name ="ajinkya";
+		boolean asd= true;
 	}
 
 }
---
+++ src/test/RegPage.java
@@ -0,0 +1,10 @@
+package test;
+
+public class RegPage {
+
+	public static void main(String[] args) {
+		int a =10;
+
+	}
+
+}
