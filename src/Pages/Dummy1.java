28a495be8289084283005f118cb53dae1d06a2af
aj <ajinkyasutar064@gmail.com>
2022-12-04 12:29:12 +0530

    first commit in git priactice

---
+++ src/Pages/Dummy1.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class Dummy1 {
+
+	public static void main(String[] args) {
+		System.out.println("good");
+	}
+
+}
---
+++ src/Pages/Dummy2.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class Dummy2 {
+	
+	public void method1(){
+		System.out.println("Practice git commands");
+	}
+
+}
