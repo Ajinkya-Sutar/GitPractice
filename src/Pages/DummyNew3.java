dc7b46325cb0dfcc70f767872f44c40eab471085
aj <ajinkyasutar064@gmail.com>
2022-12-04 13:38:10 +0530

    class updated

--- src/Pages/DummyNew3.java
+++ src/Pages/DummyNew3.java
@@ -5,6 +5,8 @@ public class DummyNew3 {
 	public static void main(String[] args) {
 	
 		System.out.println("added new one");
+		System.out.println("working and learning those thing are mandatory" );
+		String tree = "little";
 
 	}
 
